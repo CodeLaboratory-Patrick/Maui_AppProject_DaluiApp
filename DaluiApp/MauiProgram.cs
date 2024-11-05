d6f28fae280f4e8a53fa5c39e304afeed0ba3e2c
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-05 10:07:32 +0100

    Required Nuget Package : CommunityToolkit.Maui + implemented a new nuget package.

--- DaluiApp/MauiProgram.cs
+++ DaluiApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using PanCardView;
 
 namespace DaluiApp
@@ -11,6 +12,7 @@ namespace DaluiApp
             builder
                 .UseMauiApp<App>()
                 .UseCardsView()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
