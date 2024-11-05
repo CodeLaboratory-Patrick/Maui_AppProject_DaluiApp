7dc89fff7e2e2cdaf14df5f9b4c529849d08a23d
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-05 11:47:21 +0100

    implemented an image collection at the middle part.

---
+++ DaluiApp/Models/ArtStyle.cs
@@ -0,0 +1,7 @@
+namespace DaluiApp.Models;
+
+public class ArtStyle
+{
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+}
--- DaluiApp/Views/GenerationOptionsView.xaml.cs
+++ DaluiApp/Views/GenerationOptionsView.xaml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaluiApp.Models;
 
 namespace DaluiApp.Views;
 
 public partial class GenerationOptionsView : ContentPage
 {
     public List<string> Options { get; set; }
+    public List<ArtStyle> Styles { get; set; }
     public GenerationOptionsView()
     {
         InitializeComponent();
@@ -23,6 +25,19 @@ public partial class GenerationOptionsView : ContentPage
             "World",
             "Winter",
             "Trees"
+        };
+        Styles = new List<ArtStyle>()
+        {
+            new ArtStyle() { Name = "Cartoon", ImageUrl = "cartoon.jpg" },
+            new ArtStyle() { Name = "Realistic", ImageUrl = "realistic.jpg" },
+            new ArtStyle() { Name = "Watercolor Art", ImageUrl = "watercolor.jpg" },
+            new ArtStyle() { Name = "Isometric", ImageUrl = "isometric.jpg" },
+            new ArtStyle() { Name = "Pop Art", ImageUrl = "popart.jpg" },
+            new ArtStyle() { Name = "Surrealism", ImageUrl = "surrealism.jpg" },
+            new ArtStyle() { Name = "Minimalism", ImageUrl = "minimalism.jpg" },
+            new ArtStyle() { Name = "Funko", ImageUrl = "funko.jpg" },
+            new ArtStyle() { Name = "Anime", ImageUrl = "anime.jpg" },
+            new ArtStyle() { Name = "Storybook", ImageUrl = "storybook.jpg" },
         };
     }
 }
