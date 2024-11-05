d688c144ac8b5f30a1dcb7a6a2719e23dd81c161
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-05 09:45:22 +0100

    Made 2 models + the horizontal collection

---
+++ DaluiApp/Models/Profile.cs
@@ -0,0 +1,8 @@
+namespace DaluiApp.Models;
+
+public class Profile
+{
+    public string ProfileImage { get; set; }
+    public string Name { get; set; }
+    public int NoPhotos { get; set; }
+}
--- DaluiApp/Views/DashboardView.xaml.cs
+++ DaluiApp/Views/DashboardView.xaml.cs
@@ -1,15 +1,79 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaluiApp.Models;
 
 namespace DaluiApp.Views;
 
 public partial class DashboardView : ContentPage
 {
+    public ObservableCollection<Profile> Profiles { get; set; }
+    public ObservableCollection<GeneratedImage> GeneratedImages { get; set; }
+
     public DashboardView()
     {
         InitializeComponent();
+        LoadData();
+        BindingContext = this;
+    }
+
+
+    private void LoadData()
+    {
+        Profiles = new ObservableCollection<Profile>
+        {
+            new Profile
+            {
+                Name = "James",
+                ProfileImage = "profile1.jpg",
+                NoPhotos = 12
+            },
+            new Profile
+            {
+                Name = "Maddy",
+                ProfileImage = "profile2.jpg",
+                NoPhotos = 5
+            },
+            new Profile
+            {
+                Name = "Henry",
+                ProfileImage = "profile3.jpg",
+                NoPhotos = 25
+            },
+        };
+
+        GeneratedImages = new ObservableCollection<GeneratedImage>
+        {
+            new GeneratedImage
+            {
+                ImagePath = "dashboard1.jpg",
+                MainKeyword = "Castle",
+                Keywords = new List<string>
+                {
+                    "Epic, hill, mountain, trees, blue sky"
+                }
+            },
+            new GeneratedImage
+            {
+                ImagePath = "dashboard2.jpg",
+                MainKeyword = "Mountains",
+                Keywords = new List<string>
+                {
+                    "Landscape, photorealistic, dawn, mountains, moon"
+                }
+            },
+            new GeneratedImage
+            {
+                ImagePath = "dashboard3.jpg",
+                MainKeyword = "Robot",
+                Keywords = new List<string>
+                {
+                    "AI, robotic, human, light, metal"
+                }
+            },
+        };
     }
 }
