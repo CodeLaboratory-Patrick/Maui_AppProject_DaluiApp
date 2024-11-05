2724089c4e7f84a73a1beeb2101b3167fc677c4c
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-05 11:26:28 +0100

    generated "options" labels

--- DaluiApp/Views/GenerationOptionsView.xaml.cs
+++ DaluiApp/Views/GenerationOptionsView.xaml.cs
@@ -8,8 +8,21 @@ namespace DaluiApp.Views;
 
 public partial class GenerationOptionsView : ContentPage
 {
+    public List<string> Options { get; set; }
     public GenerationOptionsView()
     {
         InitializeComponent();
+        FillOptions();
+        BindingContext = this;
+    }
+
+    private void FillOptions()
+    {
+        Options = new List<string>
+        {
+            "World",
+            "Winter",
+            "Trees"
+        };
     }
 }
